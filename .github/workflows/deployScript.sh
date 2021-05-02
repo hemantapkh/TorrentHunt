36d9476ddf9d1cf848391365468d1731c5f1cca8
Hemanta Pokharel <hemantapkh@yahoo.com>
2021-05-02 21:28:51 +0545

    Add deployScript.sh

---
+++ .github/workflows/deployScript.sh
@@ -0,0 +1,4 @@
+# Due to a bug in appleboy/ssh-action@master, some commands shows error and fails to execute.
+# So, running a bash file instead of commands
+
+echo $1 > /opt/TorrentHunt/TorrentHunt/config.json  && pkill -f torrentHunt.py ; source /opt/TorrentHunt/env/bin/activate && screen -dm python3 /opt/TorrentHunt/TorrentHunt/torrentHunt.py
