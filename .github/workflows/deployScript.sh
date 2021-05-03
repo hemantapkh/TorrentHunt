8b430f6819583d01e06e82409c4746eff8176033
Hemanta Pokharel <hemantapkh@yahoo.com>
2021-05-03 15:24:24 +0545

    Fix wrong file name

--- .github/workflows/deployScript.sh
+++ .github/workflows/deployScript.sh
@@ -1,4 +1,4 @@
 # Due to a bug in appleboy/ssh-action@master, some commands shows error and fails to execute.
 # So, running a bash file instead of commands
 
-echo $1 > /opt/TorrentHunt/TorrentHunt/config.json  && pkill -f torrentHunt.py ; source /opt/TorrentHunt/env/bin/activate && screen -dm python3 /opt/TorrentHunt/TorrentHunt/torrentHunt.py
+echo $1 > /opt/TorrentHunt/TorrentHunt/config.json  && pkill -f torrenthunt.py ; source /opt/TorrentHunt/env/bin/activate && screen -dm python3 /opt/TorrentHunt/TorrentHunt/torrenthunt.py
