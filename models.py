ab83b9ec517423b59c5ea6519199bec0d3105f01
Hemanta Pokharel <hemantapkh@yahoo.com>
2021-05-04 12:58:59 +0545

    Automatically register users if not registered

--- models.py
+++ models.py
@@ -4,47 +4,49 @@ class dbQuery():
     def __init__(self, db):
         self.db = db
     
-    #: Return the userId of the user. Returns None if the user is not registered.
-    def getUserId(self, telegramId):
-        with sqlite3.connect(self.db) as con:
-            cur = con.cursor()
-            user = cur.execute(f'SELECT * FROM users WHERE telegramId={telegramId}').fetchone()
-            con.commit()
-            
-            return user[0] if user!=None else None
+    #: Add the user into the database if not registered
+    def setAccount(self, userId):
+        con = sqlite3.connect(self.db)
+        cur = con.cursor()
 
-    #: Add the user into the database and give them a unique UserId
-    def setUserId(self, telegramId):
-        with sqlite3.connect(self.db) as con:
-            cursor = con.cursor()
-            cursor.execute(f'Insert into users (telegramId) values ({telegramId})')
+        isRegistered = cur.execute(f'SELECT * FROM users WHERE userId={userId}').fetchone()
+        con.commit()
+
+        if not isRegistered:
+            cur.execute(f'Insert into users (userId) values ({userId})')
+            cur.execute(f'Insert into settings (ownerId) values ({userId})')
             con.commit()
 
     #: Get all the registered users
-    def getAllAccounts(self):
-        with sqlite3.connect(self.db) as con:
-            cur = con.cursor()
-            users = cur.execute(f'SELECT * FROM users WHERE telegramId NOT NULL').fetchall()
-            con.commit()
+    def getAllUsers(self):
+        con = sqlite3.connect(self.db)
+        con.row_factory = lambda cursor, row: row[0]
+        cur = con.cursor()
+        
+        users = cur.execute(f'SELECT * FROM users WHERE userId NOT NULL').fetchall()
+        con.commit()
 
-            return users if users else None
+        return users if users else None
 
     #: Get the user's settings
     def getSetting(self, userId, var):
-        with sqlite3.connect(self.db) as con:
-            cur = con.cursor()
-            setting = cur.execute(f'SELECT {var} FROM settings WHERE ownerId={userId} limit 1').fetchone()
-            con.commit()
+        self.setAccount(userId)
+        con = sqlite3.connect(self.db)
+        cur = con.cursor()
+        
+        setting = cur.execute(f'SELECT {var} FROM settings WHERE ownerId={userId} limit 1').fetchone()
+        con.commit()
 
-            return setting[0] if setting!=None else None
+        return setting[0] if setting else None
 
     #: Set the user's settings    
     def setSetting(self, userId, var, value):
-        with sqlite3.connect(self.db) as con:
-            cur = con.cursor()
+        self.setAccount(userId)
+        con = sqlite3.connect(self.db)
+        cur = con.cursor()
 
-            #!? If value is None, put value as NULL else "{string}"
-            value = f'"{value}"' if value else 'NULL'
-            cur.execute(f'INSERT OR IGNORE INTO settings (ownerId, {var}) VALUES ({userId}, {value})')
-            cur.execute(f'UPDATE settings SET {var}={value} WHERE ownerId={userId}')
-            con.commit()
+        #!? If value is None, put value as NULL else "{string}"
+        value = f'"{value}"' if value else 'NULL'
+        cur.execute(f'INSERT OR IGNORE INTO settings (ownerId, {var}) VALUES ({userId}, {value})')
+        cur.execute(f'UPDATE settings SET {var}={value} WHERE ownerId={userId}')
+        con.commit()
