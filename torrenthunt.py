4654cb0d81368274a679af76676edf1dd2cf0376
Hemanta Pokharel <hemantapkh@yahoo.com>
2021-07-17 22:38:51 +0545

    Add start parameter to search torrents

--- torrenthunt.py
+++ torrenthunt.py
@@ -1,5 +1,5 @@
 import requests
-import json, ssl
+import json, ssl, base64
 from pathlib import Path
 from os import path, remove
 from ast import literal_eval
@@ -225,7 +225,23 @@ def result(response, userLanguage, torrentType, page, category=None, week=None, 
 def start(message):
     if dbSql.setAccount(message.from_user.id):
         userLanguage = dbSql.getSetting(message.from_user.id, 'language')
-        bot.send_message(message.chat.id, text=language['greet'][userLanguage].format(message.from_user.first_name), reply_markup=mainReplyKeyboard(userLanguage), disable_web_page_preview=True)
+        params = message.text.split()[1] if len(message.text.split()) > 1 else None
+
+        #! If start paramater is passed
+        if params:
+            try:
+                text = base64.b64decode(params.encode('utf')).decode('utf')
+                sent = bot.send_message(message.chat.id, language['searchingQuery'][userLanguage].format(text))
+                response = torrent.search(text)
+
+                msg, markup = result(response, userLanguage, torrentType='query', page=1, query=text)
+
+                bot.edit_message_text(chat_id=message.chat.id, message_id=sent.message_id, text=msg or language['noResults'][userLanguage], reply_markup=markup)
+            
+            except Exception:
+                bot.send_message(message.chat.id, text=language['greet'][userLanguage].format(message.from_user.first_name), reply_markup=mainReplyKeyboard(userLanguage), disable_web_page_preview=True)
+        else:
+            bot.send_message(message.chat.id, text=language['greet'][userLanguage].format(message.from_user.first_name), reply_markup=mainReplyKeyboard(userLanguage), disable_web_page_preview=True)
     else:
         lang(message, userLanguage='english', greet=True)
 
