60cc4faa0f870421979e97dfbca3c83d7b75c39d
Hemanta Pokharel <hemantapkh@yahoo.com>
2021-07-30 19:38:05 +0545

    Add extra suggestion button

--- torrenthunt.py
+++ torrenthunt.py
@@ -44,6 +44,7 @@ def shortner(url):
     short = requests.get(f'http://tinyurl.com/api-create.php?url={url}')
     return short.text
 
+#: Get suggestion query
 def getSuggestions(query):
     headers = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0'}
 
@@ -55,6 +56,11 @@ def getSuggestions(query):
     response = requests.get('https://www.google.com/complete/search', headers=headers, params=params)
     
     return literal_eval(response.text)[1]
+
+#: Sort list according to the length of elements
+def sortList(lst):
+    lst2 = sorted(lst, key=len)
+    return lst2
 
 # Main reply keyboard
 def mainReplyKeyboard(userLanguage):
@@ -644,16 +650,43 @@ def text(message):
             try:
                 suggestion = getSuggestions(message.text)
                 
-                if suggestion and suggestion[0] != message.text:
-                    bot.edit_message_text(chat_id=message.chat.id, message_id=sent.message_id, text=language['searchingQuery'][userLanguage].format(suggestion[0]))
-                    response = torrent.search(suggestion[0])
-                    
-                    msg, markup = result(response, userLanguage, torrentType='query', page=1, query=suggestion[0])
+                if suggestion:
+                    if suggestion[0] != message.text:
+                        bot.edit_message_text(chat_id=message.chat.id, message_id=sent.message_id, text=language['searchingQuery'][userLanguage].format(suggestion[0]))
+                        response = torrent.search(suggestion[0])
+                        
+                        msg, markup = result(response, userLanguage, torrentType='query', page=1, query=suggestion[0])
+
+                    if not msg:
+                        markup.row_width = 3
+                        suggestion = sortList(suggestion)
+                        buttons = []
+                        smallButtons = []
+                        
+                        for i in suggestion[1:]:
+                            suggestedQuery = base64.b64encode(i.encode()).decode()
+                            if len(suggestedQuery) <= 64:
+
+                                if len(i) < 13:
+                                    smallButtons.append(telebot.types.InlineKeyboardButton(text=i, url=f"https://t.me/torrenthuntbot?start={suggestedQuery}"))
+                                
+                                elif len(i) < 18:
+                                    buttons.append(telebot.types.InlineKeyboardButton(text=i, url=f"https://t.me/torrenthuntbot?start={suggestedQuery}"))
+                                
+                                else:
+                                    markup.add(telebot.types.InlineKeyboardButton(text=i, url=f"https://t.me/torrenthuntbot?start={suggestedQuery}"))
+
+                        
+                        markup.add(*smallButtons)
+                        markup.row_width = 2
+                        markup.add(*buttons)
+                        
                     bot.edit_message_text(chat_id=message.chat.id, message_id=sent.message_id, text=msg or language['noResults'][userLanguage], reply_markup=markup)
+                
                 else:
                     bot.edit_message_text(chat_id=message.chat.id, message_id=sent.message_id, text=language['noResults'][userLanguage], reply_markup=markup)
             
-            except Exception:
+            except Exception as e:
                 bot.edit_message_text(chat_id=message.chat.id, message_id=sent.message_id, text=msg, reply_markup=markup)
         else:
             bot.edit_message_text(chat_id=message.chat.id, message_id=sent.message_id, text=msg, reply_markup=markup)
