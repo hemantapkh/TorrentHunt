651499893b17a5b6f3e4d6c8aed34302fab2bec9
Hemanta Pokharel <hemantapkh@yahoo.com>
2022-04-02 15:18:26 +0545

    Replace sqlite3 with psycpg2

--- models.py
+++ models.py
@@ -1,118 +1,77 @@
-import sqlite3
+import psycopg2
 import uuid, time
 from datetime import datetime
 
 class dbQuery():
     def __init__(self, db, mdb):
-        self.db = db
-        self.mdb = mdb
-    
+        conn = psycopg2.connect(user="postgres", password="postgres", host="127.0.0.1", port="5432", database=db)
+        conn.autocommit = True
+        self.cur = conn.cursor()
+
+        conn = psycopg2.connect(user="postgres", password="postgres", host="127.0.0.1", port="5432", database=mdb)
+        conn.autocommit = True
+        self.mcur = conn.cursor()
+
     #: Add the user into the database if not registered
     def setAccount(self, userId, userName=None):
         chatType = 'users' if userId > 0 else 'groups'
-        con = sqlite3.connect(self.db)
-        cur = con.cursor()
 
-        isRegistered = self.isRegistered(userId, chatType)
+        self.cur.execute(f"""Insert into {chatType}
+                        (userId, userName, date) values 
+                        ({userId}, '{userName}', '{datetime.today().strftime("%Y-%m-%d")}')
+                        ON CONFLICT (userId) DO UPDATE SET userId=EXCLUDED.userId 
+                        RETURNING userId
+                        """)
 
-        if not isRegistered:
-            if chatType == 'users':
-                cur.execute(f"Insert into {chatType} (userId, date) values ({userId}, \"{datetime.today().strftime('%Y-%m-%d')}\")")
-                cur.execute(f'Insert into flood (ownerId) values ({userId})')
-
-            else:
-                cur.execute(f"Insert into {chatType} (userId, userName, date) values ({userId}, \"{userName}\", \"{datetime.today().strftime('%Y-%m-%d')}\")")
-            
-            cur.execute(f'Insert into settings (ownerId) values ({userId})')
+        isRegistered = self.cur.fetchone()
                         
-            con.commit()
-        
-        return isRegistered
-
-    #: Find if a user is registered or not
-    def isRegistered(self, userId, chatType='users'):
-        con = sqlite3.connect(self.db)
-        cur = con.cursor()
-
-        isRegistered = cur.execute(f'SELECT * FROM {chatType} WHERE userId={userId}').fetchone()
-        con.commit()
-
         return True if isRegistered else False
 
-    #: Get all the registered users
-    def getAllUsers(self, type='users', date=None, countOnly=False):
-        con = sqlite3.connect(self.db)
-        con.row_factory = lambda cursor, row: row[0]
-        cur = con.cursor()
-        
+    #: Get user stats
+    def getUsers(self, type='users', language=None, languageExcept=None, date=None, countOnly=False):
         if countOnly:
-            if date:
-                users = cur.execute(f'SELECT count(*) FROM {type} WHERE DATE="{date}"').fetchone()
-            else:
-                users = cur.execute(f'SELECT count(*) FROM {type}').fetchone()
-
+            self.cur.execute(f"""SELECT count(*) FROM settings 
+                                FULL OUTER JOIN USERS on ownerId=userId
+                                WHERE
+                                    language={f"'{language}'" if language else 'language'} AND
+                                    date={f"'{date}'" if date else 'date'}
+                                    {f"AND language NOT IN {str(languageExcept).replace('[','(').replace(']',')')}" if languageExcept else ''}  
+            """)
+            
+            users = self.cur.fetchone()[0]
+        
         else:
-            users = cur.execute(f'SELECT userId FROM {type}').fetchall()
-        
-        con.commit()
-        return users
-    
-    #: Get all users exclude certain languages
-    #: languages must inside a list
-    def getUsersExcept(self, languages):
-        con = sqlite3.connect(self.db)
-        con.row_factory = lambda cursor, row: row[0]
-        cur = con.cursor()
-        
-        users = cur.execute(f"SELECT ownerId FROM settings WHERE language NOT IN {str(languages).replace('[','(').replace(']',')')}").fetchall()
-        con.commit()
+            self.cur.execute(f"""
+                    SELECT userId FROM settings 
+                    FULL OUTER JOIN USERS on ownerId=userId
+                    WHERE
+                        language={f"'{language}'" if language else 'language'} AND
+                        date={f"'{date}'" if date else 'date'}
+                        {f"AND language NOT IN {str(languageExcept).replace('[','(').replace(']',')')}" if languageExcept else ''} 
+            """)
+            
+            users = self.cur.fetchall()
 
-        return users
-    
-    #: Get users of particular language
-    def getUsers(self, language, countOnly=False):
-        con = sqlite3.connect(self.db)
-        con.row_factory = lambda cursor, row: row[0]
-        cur = con.cursor()
-        
-        if countOnly:
-            users = cur.execute(f'SELECT count(*) FROM settings WHERE language="{language}"').fetchone()
-        else:
-            users = cur.execute(f'SELECT ownerId FROM settings WHERE language="{language}"').fetchall()
-        
-        con.commit()
         return users
 
     #: Get the user's settings
     def getSetting(self, userId, var, table='settings'):
-        self.setAccount(userId)
-        con = sqlite3.connect(self.db)
-        cur = con.cursor()
-        
-        setting = cur.execute(f'SELECT {var} FROM {table} WHERE ownerId={userId} limit 1').fetchone()
-        con.commit()
+        self.cur.execute(f'SELECT {var} FROM {table} WHERE ownerId={userId} limit 1')
+        setting = self.cur.fetchone()
 
         return setting[0] if setting else None
 
     #: Set the user's settings    
     def setSetting(self, userId, var, value, table='settings'):
         self.setAccount(userId)
-        con = sqlite3.connect(self.db)
-        cur = con.cursor()
 
-        #!? If value is None, put value as NULL else "{string}"
-        value = f'"{value}"' if value else 'NULL'
-        cur.execute(f'INSERT OR IGNORE INTO {table} (ownerId, {var}) VALUES ({userId}, {value})')
-        cur.execute(f'UPDATE {table} SET {var}={value} WHERE ownerId={userId}')
-        con.commit()
+        self.cur.execute(f'''INSERT INTO {table} (ownerId, {var}) VALUES ({userId}, '{value}') 
+                        ON CONFLICT (ownerId) 
+                        DO UPDATE SET {var}='{value}' WHERE EXCLUDED.ownerId={userId}''')
 
     #: Set magnet link in the database
     def setMagnet(self, magnetLink):
-        con = sqlite3.connect(self.mdb)
-        cur = con.cursor()
-
         key = uuid.uuid4().hex
-        cur.execute(f'Insert into data (key, date, magnetLink) VALUES ("{key}", {int(time.time())}, "{magnetLink}")')
-        con.commit()
+        self.cur.execute(f"Insert into data (key, date, magnetLink) VALUES ('{key}', {int(time.time())}, '{magnetLink}')")
 
         return key
