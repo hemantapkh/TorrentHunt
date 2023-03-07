a20d3fb7f08c9d86e5dfd9f4486cb46dc0a8da55
Hemanta Pokharel <hemantapkh@yahoo.com>
2023-03-07 21:47:48 +0545

    Fix error in deploy command

--- .github/workflows/deployScript.sh
+++ .github/workflows/deployScript.sh
@@ -1,4 +1,4 @@
 # Due to a bug in appleboy/ssh-action@master, some commands shows error and fails to execute.
 # So, running a bash file instead of commands
 
-echo $1 > /opt/TorrentHunt/TorrentHunt/src/config.json ; source /opt/TorrentHunt/venv/bin/activate && && cd /opt/TorrentHunt/TorrentHunt/ && pip3 install -r requirements.txt ; pm2 restart TorrentHunt
+echo $1 > /opt/TorrentHunt/TorrentHunt/src/config.json ; source /opt/TorrentHunt/venv/bin/activate && cd /opt/TorrentHunt/TorrentHunt/ && pip3 install -r requirements.txt ; pm2 restart TorrentHunt
