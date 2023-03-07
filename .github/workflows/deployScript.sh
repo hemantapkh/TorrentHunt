02d5446d4176bc26f30d26c524261874547b2f66
Hemanta Pokharel <hemantapkh@yahoo.com>
2023-03-07 21:42:50 +0545

    Use pm2 instead of screen

--- .github/workflows/deployScript.sh
+++ .github/workflows/deployScript.sh
@@ -1,4 +1,4 @@
 # Due to a bug in appleboy/ssh-action@master, some commands shows error and fails to execute.
 # So, running a bash file instead of commands
 
-echo $1 > /opt/TorrentHunt/TorrentHunt/src/config.json  && pkill -f torrenthunt.py ; source /opt/TorrentHunt/venv/bin/activate && screen -dm python3 /opt/TorrentHunt/TorrentHunt/torrenthunt.py
+echo $1 > /opt/TorrentHunt/TorrentHunt/src/config.json ; source /opt/TorrentHunt/venv/bin/activate && && cd /opt/TorrentHunt/TorrentHunt/ && pip3 install -r requirements.txt ; pm2 reload torrenthunt
