adf009436d9c26566aa9bf15c243aecdab306011
Hemanta Pokharel <hemanta.pokharel@docsumo.com>
2023-03-12 22:52:22 +0545

    Add set_user method in database API

--- apis/database.py
+++ apis/database.py
@@ -9,6 +9,7 @@ class DataBase:
         self.password = password
         self.database = database
 
+    # Query on the database
     async def query(self, method, query, *args):
         con = await asyncpg.connect(
             user=self.user,
@@ -19,3 +20,29 @@ class DataBase:
 
         await con.close()
         return row
+
+    # Insert user into database
+    async def set_user(self, message, referrer=None):
+        query = '''
+        INSERT INTO USERS
+            (user_id, user_type, username, first_name, last_name, referrer)
+            VALUES ($1, $2, $3, $4, $5, $6)
+        ON CONFLICT (user_id)
+        DO NOTHING
+        '''
+
+        # If chat type if group/channel
+        if message.chat.type.name != 'PRIVATE':
+            message.chat.first_name = message.chat.title
+            message.chat.last_name = None
+
+        await self.query(
+            'execute',
+            query,
+            message.chat.id,
+            message.chat.type.name,
+            message.chat.username,
+            message.chat.first_name,
+            message.chat.last_name,
+            str(referrer) if referrer else None,
+        )
