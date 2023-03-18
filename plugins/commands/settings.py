dc38678b1e7dc163e6b06b39b4ad992ee04600d9
Hemanta Pokharel <hemanta.pokharel@docsumo.com>
2023-03-18 14:03:25 +0545

    Add settings command

---
+++ plugins/commands/settings.py
@@ -0,0 +1,29 @@
+from pyrogram import Client, filters, types
+
+
+@Client.on_message(filters.command('settings'))
+async def settings(Client, message):
+    user_lang = await Client.MISC.user_lang(message)
+
+    buttons = [
+        [
+            types.InlineKeyboardButton(
+                text=Client.LG.BTN('languageSetting', user_lang),
+                callback_data='language',
+            ),
+        ],
+        [
+            types.InlineKeyboardButton(
+                text=Client.LG.BTN('turnOnRestrictedMode', user_lang),
+                callback_data='restricted_mode',
+            ),
+        ],
+    ]
+
+    await Client.send_message(
+        chat_id=message.chat.id,
+        text=Client.LG.STR('settings', user_lang).format(
+            Client.LG.CMD('settings', user_lang),
+        ),
+        reply_markup=types.InlineKeyboardMarkup(buttons),
+    )
