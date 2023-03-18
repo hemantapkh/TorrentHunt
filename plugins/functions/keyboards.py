d8f3acae1b71a1c28c4ce62d0301a859bf466504
Hemanta Pokharel <hemanta.pokharel@docsumo.com>
2023-03-18 14:02:21 +0545

    Add keyboard functions

--- plugins/functions/keyboards.py
+++ plugins/functions/keyboards.py
@@ -7,14 +7,56 @@ class KeyBoard:
     def __init__(self, client):
         self.client = client
 
-    async def main(self, message):
+    async def main(self, lang):
         return types.ReplyKeyboardMarkup(
             [
                 [
-                    await self.client.LG.BTN('settings', message),
-                    await self.client.LG.BTN('help', message),
-                    await self.client.LG.BTN('support', message),
+                    self.client.LG.BTN('settings', lang),
+                    self.client.LG.BTN('help', lang),
+                    self.client.LG.BTN('support', lang),
                 ],
             ],
             resize_keyboard=True,
+        )
+
+    async def sites(self, keyword):
+        results = [
+            types.InlineKeyboardButton(
+                text=self.client.sites[key]['title'],
+                switch_inline_query_current_chat=f"{self.client.sites[key]['code']} {keyword}",
+            )
+
+            for key in
+            self.client.sites if not self.client.sites[key].get('deactivated')
+        ]
+
+        return types.InlineKeyboardMarkup(
+            self.client.MISC.split_list(results, 3),
+        )
+
+    def language(self):
+        results = [
+            types.InlineKeyboardButton(
+                text=self.client.LG.config[key]['title'],
+                callback_data=f"setLanguage_{self.client.LG.config[key]['code']}",
+            )
+
+            for key in
+            self.client.LG.config
+        ]
+
+        return types.InlineKeyboardMarkup(
+            self.client.MISC.split_list(results, 2),
+        )
+
+    def torrent_info(self, hash):
+        results = [
+            types.InlineKeyboardButton(
+                text=self.client.LG.BTN('addToSeedr'),
+                url=f'https://t.me/torrentseedrbot?start=addTorrent_{hash}',
+            ),
+        ]
+
+        return types.InlineKeyboardMarkup(
+            [results],
         )
