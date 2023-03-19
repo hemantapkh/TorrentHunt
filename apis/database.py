083519a370dca64ff1563582d228d55da0cc93ea
Hemanta Pokharel <hemanta.pokharel@docsumo.com>
2023-03-19 16:15:00 +0545

    Update set_user API

--- apis/database.py
+++ apis/database.py
@@ -24,11 +24,17 @@ class DataBase:
     # Insert user into database
     async def set_user(self, message, referrer=None):
         query = '''
-        INSERT INTO USERS
+        INSERT INTO users
             (user_id, user_type, username, first_name, last_name, referrer)
-            VALUES ($1, $2, $3, $4, $5, $6)
+        VALUES
+            ($1, $2, $3, $4, $5, $6)
         ON CONFLICT (user_id)
-        DO NOTHING
+        DO UPDATE SET
+            user_type = $2,
+            username = $3,
+            first_name = $4,
+            last_name = $5
+        RETURNING CASE xmax WHEN 0 THEN TRUE ELSE FALSE END;
         '''
 
         # If chat type if group/channel
@@ -36,8 +42,8 @@ class DataBase:
             message.chat.first_name = message.chat.title
             message.chat.last_name = None
 
-        await self.query(
-            'execute',
+        res = await self.query(
+            'fetchval',
             query,
             message.chat.id,
             message.chat.type.name,
@@ -46,3 +52,5 @@ class DataBase:
             message.chat.last_name,
             str(referrer) if referrer else None,
         )
+
+        return True if res == 'INSERT 0 1' else False
