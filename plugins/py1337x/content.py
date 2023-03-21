c3fa2124c4e3fd02a1033e0fa2f1a0b736c9598b
Hemanta Pokharel <hemanta.pokharel@docsumo.com>
2023-03-21 21:21:30 +0545

    Fix getLink on groups

--- plugins/py1337x/content.py
+++ plugins/py1337x/content.py
@@ -4,11 +4,12 @@ from pyrogram import Client, filters
 @Client.on_message(filters.regex('getLink'))
 async def results(Client, message):
     user_lang = await Client.MISC.user_lang(message)
-    torrent_id = message.text.split('_')[1]
+    torrent_id = message.text.split('_')[1].split('@')[0]
 
     msg = await Client.send_message(
         chat_id=message.chat.id,
         text=Client.LG.STR('fetchingTorrentInfo', user_lang),
+        reply_to_message_id=message.id,
     )
 
     response = Client.py1337x.info(torrentId=torrent_id)
