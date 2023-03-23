2226b9924527a03f8b09546f32c864a64c8c6b87
Hemanta Pokharel <hemanta.pokharel@docsumo.com>
2023-03-23 22:30:45 +0545

    Create trigger for settings on users

--- sql/postgres.sql
+++ sql/postgres.sql
@@ -1,3 +1,4 @@
+-- Table for users
 CREATE TABLE IF NOT EXISTS USERS(
     user_id         BIGINT PRIMARY KEY,
     user_type       TEXT NOT NULL,
@@ -9,6 +10,7 @@ CREATE TABLE IF NOT EXISTS USERS(
     last_active     TIMESTAMP DEFAULT current_timestamp
 );
 
+-- Table for storing user settings
 CREATE TABLE IF NOT EXISTS SETTINGS(
     user_id         BIGINT PRIMARY KEY,
     language        TEXT DEFAULT 'english',
@@ -16,12 +18,27 @@ CREATE TABLE IF NOT EXISTS SETTINGS(
     FOREIGN KEY     (user_id) REFERENCES USERS(user_id) ON DELETE CASCADE
 );
 
+-- Trigger function to insert a new row into SETTINGS when a new row is inserted into USERS
+CREATE FUNCTION insert_default_settings() RETURNS TRIGGER AS $$
+BEGIN
+  INSERT INTO SETTINGS (user_id) VALUES (NEW.user_id);
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Trigger to call the function above
+CREATE TRIGGER user_insert_trigger AFTER INSERT ON USERS
+FOR EACH ROW EXECUTE FUNCTION insert_default_settings();
+
+
+-- Table for admins
 CREATE TABLE IF NOT EXISTS ADMINS(
     user_id         BIGINT PRIMARY KEY,
     date            TIMESTAMP DEFAULT current_timestamp,
     FOREIGN KEY     (user_id) REFERENCES USERS(user_id) ON DELETE CASCADE
 );
 
+-- Table for creating tracking links
 CREATE TABLE IF NOT EXISTS REFERRERS(
     referrer_id     TEXT PRIMARY KEY,
     title           TEXT NOT NULL,
