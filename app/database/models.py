5b34a94130d12467b94b2a1f743bb3d0c9eae540
Hemanta Pokharel <hemanta.pokharel@docsumo.com>
2024-01-27 20:54:35 +0545

    Fix current_timestamp

--- app/database/models.py
+++ app/database/models.py
@@ -3,6 +3,7 @@ import asyncio
 from sqlalchemy import TIMESTAMP, Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+from sqlalchemy.sql import func
 
 Base = declarative_base()
 
@@ -21,8 +22,8 @@ class User(Base):
     first_name = Column(String)
     last_name = Column(String)
     referrer = Column(String)
-    join_date = Column(TIMESTAMP, server_default="CURRENT_TIMESTAMP")
-    last_active = Column(TIMESTAMP, server_default="CURRENT_TIMESTAMP")
+    join_date = Column(TIMESTAMP, server_default=func.current_timestamp())
+    last_active = Column(TIMESTAMP, server_default=func.current_timestamp())
 
     settings = relationship("Settings", uselist=False, back_populates="user")
 
@@ -48,7 +49,7 @@ class Bookmark(Base):
     leechers = Column(String)
     size = Column(String)
     uploaded_on = Column(String)
-    date = Column(TIMESTAMP, server_default="CURRENT_TIMESTAMP")
+    date = Column(TIMESTAMP, server_default=func.current_timestamp())
 
     user = relationship("User", backref="bookmarks")
 
@@ -57,7 +58,7 @@ class Admin(Base):
     __tablename__ = "ADMINS"
 
     user_id = Column(Integer, primary_key=True)
-    date = Column(TIMESTAMP, server_default="CURRENT_TIMESTAMP")
+    date = Column(TIMESTAMP, server_default=func.current_timestamp())
 
 
 class Referrer(Base):
@@ -67,7 +68,7 @@ class Referrer(Base):
     title = Column(String, nullable=False)
     description = Column(String)
     clicks = Column(Integer, nullable=False, default=0)
-    date = Column(TIMESTAMP, server_default="CURRENT_TIMESTAMP")
+    date = Column(TIMESTAMP, server_default=func.current_timestamp())
 
 
 async def init_models():
