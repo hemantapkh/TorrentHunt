dab880e120884a2acfa15da23c8db5c612375687
Hemanta Pokharel <hemanta.pokharel@docsumo.com>
2024-01-28 10:08:53 +0545

    Fix torrent info

--- app/plugins/py1337x/content.py
+++ app/plugins/py1337x/content.py
@@ -1,19 +1,21 @@
+from database.models import Setting
 from pyrogram import Client, filters
+from sqlalchemy import select
 
 
-@Client.on_message(filters.regex('getLink'))
+@Client.on_message(filters.regex("getLink"))
 async def results(Client, message):
     user_lang = await Client.MISC.user_lang(message)
-    restricted_mode = await Client.DB.query(
-        'fetchval',
-        'SELECT restricted_mode FROM settings WHERE user_id = $1',
-        message.chat.id,
-    )
-    torrent_id = message.text.split('_')[1].split('@')[0]
+
+    query = select(Setting.restricted_mode).where(Setting.user_id == message.chat.id)
+    restricted_mode = await Client.DB.execute(query)
+    restricted_mode = restricted_mode.scalar()
+
+    torrent_id = message.text.split("_")[1].split("@")[0]
 
     msg = await Client.send_message(
         chat_id=message.chat.id,
-        text=Client.LG.STR('fetchingTorrentInfo', user_lang),
+        text=Client.LG.STR("fetchingTorrentInfo", user_lang),
         reply_to_message_id=message.id,
     )
 
