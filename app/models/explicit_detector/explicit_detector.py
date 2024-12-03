d7171bffb092837f16da16a2a214dbbe96dd219e
Hemanta Pokharel <hemanta.pokharel@docsumo.com>
2024-12-03 22:49:57 +0900

    Use joblib

--- app/models/explicit_detector/explicit_detector.py
+++ app/models/explicit_detector/explicit_detector.py
@@ -1,19 +1,17 @@
-import pickle
-
-
+import joblib
 
 class ExplicitDetector:
-    def __init__(self, model: str = "GaussianNB.pickle"):
-        self.vectorizer = pickle.load(
-            open("models/explicit_detector/vectorizer.pickle", "rb"),
+    def __init__(self, model: str = "Randomforest.joblib"):
+        self.vectorizer = joblib.load(
+            open("models/explicit_detector/vectorizer.joblib", "rb"),
         )
-        self.model = pickle.load(
+        self.model = joblib.load(
             open(f"models/explicit_detector/{model}", "rb"),
         )
 
     def predict(self, text):
         try:
-            vector = self.vectorizer.transform([text]).toarray()
-            return self.model.predict(vector)
+            vector = self.vectorizer.transform([text])
+            return bool(self.model.predict(vector)[0])
         except Exception:
             return False
